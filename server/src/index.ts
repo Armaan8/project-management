270df427a98f5c5b2e5ea9022710557589cc0902
Armaan <armaansharma081003@gmail.com>
2025-04-16 21:09:36 +0530

    server mod

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-/* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
 import userRoutes from "./routes/userRoutes";
 import teamRoutes from "./routes/teamRoutes";
 
-/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,7 +20,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/* ROUTES */
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
@@ -33,7 +30,6 @@ app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
-/* SERVER */
 const port = Number(process.env.PORT) || 8000;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
